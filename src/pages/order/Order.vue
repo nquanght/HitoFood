38688583278382ce5faf7e74c9cb2fb4a83f5cf7
Quang <n.nquanght@gmail.com>
2023-12-28 00:52:18 +0700

    build FE Order page

--- src/pages/order/Order.vue
+++ src/pages/order/Order.vue
@@ -1,6 +1,136 @@
 <template>
-  <div>
-    <h1>This is an order page</h1>
+  <div class="card my-3">
+    <img src="../../assets/merchant_logo/shopeefood.png" class="img-fluid rounded-start mx-3 py-2" alt="" width="72" height="72">
+    <div class="row g-0 pb-3">
+      <div class="col-md-4">
+        <img src="https://static.mservice.io/blogscontents/momo-upload-api-190919103148-637044859082271752.jpg" class="img-fluid rounded-start px-3" alt="...">
+      </div>
+      <div class="col-md-8">
+        <div class="card-body">
+          <div class="card-title mb-3 fw-bold h4">Mì trộn Nam Kỳ - Phan Văn Trị</div>
+
+          <div>
+            <div class="mb-1">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
+                <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10m0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6"/>
+              </svg>
+              <span style="padding-left: 10px"> <a>2-2A Trần Hưng Đạo, P. Bến Thành, Quận 1, TP. HCM</a></span>
+            </div>
+
+            <div class="mb-1">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-globe" viewBox="0 0 16 16">
+                <path d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m7.5-6.923c-.67.204-1.335.82-1.887 1.855A7.97 7.97 0 0 0 5.145 4H7.5zM4.09 4a9.267 9.267 0 0 1 .64-1.539 6.7 6.7 0 0 1 .597-.933A7.025 7.025 0 0 0 2.255 4zm-.582 3.5c.03-.877.138-1.718.312-2.5H1.674a6.958 6.958 0 0 0-.656 2.5zM4.847 5a12.5 12.5 0 0 0-.338 2.5H7.5V5zM8.5 5v2.5h2.99a12.495 12.495 0 0 0-.337-2.5zM4.51 8.5a12.5 12.5 0 0 0 .337 2.5H7.5V8.5zm3.99 0V11h2.653c.187-.765.306-1.608.338-2.5zM5.145 12c.138.386.295.744.468 1.068.552 1.035 1.218 1.65 1.887 1.855V12zm.182 2.472a6.696 6.696 0 0 1-.597-.933A9.268 9.268 0 0 1 4.09 12H2.255a7.024 7.024 0 0 0 3.072 2.472M3.82 11a13.652 13.652 0 0 1-.312-2.5h-2.49c.062.89.291 1.733.656 2.5zm6.853 3.472A7.024 7.024 0 0 0 13.745 12H11.91a9.27 9.27 0 0 1-.64 1.539 6.688 6.688 0 0 1-.597.933M8.5 12v2.923c.67-.204 1.335-.82 1.887-1.855.173-.324.33-.682.468-1.068zm3.68-1h2.146c.365-.767.594-1.61.656-2.5h-2.49a13.65 13.65 0 0 1-.312 2.5zm2.802-3.5a6.959 6.959 0 0 0-.656-2.5H12.18c.174.782.282 1.623.312 2.5zM11.27 2.461c.247.464.462.98.64 1.539h1.835a7.024 7.024 0 0 0-3.072-2.472c.218.284.418.598.597.933zM10.855 4a7.966 7.966 0 0 0-.468-1.068C9.835 1.897 9.17 1.282 8.5 1.077V4z"/>
+              </svg>
+              <span style="padding-left: 10px"> <a href="https://shopeefood.vn/ho-chi-minh/ga-ran-ben-thanh">https://shopeefood.vn/ho-chi-minh/ga-ran-ben-thanh</a></span>
+            </div>
+
+            <div class="mb-1">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clock-fill" viewBox="0 0 16 16">
+                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71z"/>
+              </svg>
+              <span style="padding-left: 10px"> <a>Opening hours: Today  07:00 ~ 23:00</a></span>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="row mb-3">
+    <div class="col-sm-2 fw-bold fs-5">Category</div>
+    <div class="col-sm-7 fw-bold fs-5">Discount meal</div>
+    <div class="col-sm-3 fw-bold fs-5">Ordered</div>
+  </div>
+
+  <div class="row">
+    <div class="col-sm-2">
+      <div class="card px-3 py-2">
+        <span class="my-1 py-1 px-2 bg-menu fw-bold rounded cursor-pointer">Món đang giảm</span>
+        <span class="my-1 py-1 px-2 cursor-pointer">Combo rush hour</span>
+        <span class="my-1 py-1 px-2 cursor-pointer">Món thêm</span>
+        <span class="my-1 py-1 px-2 cursor-pointer">Combo 1 gfdg fdgfdgfdgfd gfd gfdg</span>
+      </div>
+    </div>
+    <div class="col-sm-7">
+      <div class="row">
+
+        <div class="col-sm-4 mb-3">
+          <div class="card">
+            <img src="https://static.vinwonders.com/production/ga-ran-nha-trang-1.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <p class="card-text fw-bold">Gà rán KFC</p>
+              <div class="fs-price d-flex justify-content-between">
+                <div class="fw-bold">49.000 đ</div>
+                <div class="btn-group" role="group" aria-label="Second group">
+                  <div class="btn-sm btn-minus">-</div>
+                  <div class="mx-3 fw-bold" style="line-height: 20px">2</div>
+                  <div class="btn-sm btn-add">+</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-sm-4 mb-3">
+          <div class="card">
+            <img src="https://static.vinwonders.com/production/ga-ran-nha-trang-1.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <p class="card-text fw-bold">Gà rán KFC</p>
+              <div class="fs-price d-flex justify-content-between">
+                <div class="fw-bold">49.000 đ</div>
+                <div class="btn-add">+</div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-sm-4 mb-3">
+          <div class="card">
+            <img src="https://static.vinwonders.com/production/ga-ran-nha-trang-1.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <p class="card-text fw-bold">Gà rán KFC</p>
+              <div class="fs-price d-flex justify-content-between">
+                <div class="fw-bold">49.000 đ</div>
+                <div class="btn-add">+</div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-sm-4 mb-3">
+          <div class="card">
+            <img src="https://static.vinwonders.com/production/ga-ran-nha-trang-1.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <p class="card-text fw-bold">Gà rán KFC</p>
+              <div class="fs-price d-flex justify-content-between">
+                <div class="fw-bold">49.000 đ</div>
+                <div class="btn-add">+</div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-sm-4 mb-3">
+          <div class="card">
+            <img src="https://static.vinwonders.com/production/ga-ran-nha-trang-1.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <p class="card-text fw-bold">Gà rán KFC</p>
+              <div class="fs-price d-flex justify-content-between">
+                <div class="fw-bold">49.000 đ</div>
+                <div class="btn-add">+</div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+    <div class="col-sm-3">
+      <div class="card px-3 py-2">
+        <span>gdfdfg</span>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -8,5 +138,41 @@
 </script>
 
 <style scoped>
+.card {
+  border: none;
+}
 
+.bg-menu {
+  background-color: #ccffcf;
+}
+
+.cursor-pointer:hover {
+  cursor: pointer;
+}
+
+.btn-add {
+  text-align: center;
+  color: white;
+  font-size: 18px;
+  cursor: pointer;
+  font-weight: 600;
+  line-height: 18px;
+  width: 20px;
+  height: 20px;
+  background-color: #ee4d2d;
+  border-radius: 5px;
+}
+
+.btn-minus {
+  text-align: center;
+  color: white;
+  font-size: 18px;
+  cursor: pointer;
+  font-weight: 600;
+  line-height: 18px;
+  width: 20px;
+  height: 20px;
+  background-color: dimgray;
+  border-radius: 5px;
+}
 </style>
